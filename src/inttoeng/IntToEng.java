2e629f4f08ed7d9b1d23e0dde9f2214d1b02a56a
g13903 <g13903am+git@gm.tsuda.ac.jp>
2015-05-08 16:01:55 +0900

    0-1000000000まで対応可能にしました。 ドライバ　秋好

--- src/inttoeng/IntToEng.java
+++ src/inttoeng/IntToEng.java
@@ -18,66 +18,57 @@ public class IntToEng {
     public static String translateEng(int n) {
     	String s = "";
     	if(n==0) s = "zero";
-    	if(n<0||n>100000) {
+    	if(n<0||n>1000000000) {
     		s="”ÍˆÍŠO‚Å‚·";
     		n=0;
     	}
-    	String zeroToNine[]={"","one","two","three","four","five","six","seven","eight","nine"};
-    	String tenToNineteen[]={"ten","eleven","twelve","thirteen","fourteen","fifteen","seventeen","eighteen","nineteen"};
-    	String tenToNinety[]={"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
-    	int sub = 0;
+    	String zeroTo9[]={"","one","two","three","four","five","six","seven","eight","nine"};
+    	String tenTo19[]={"ten","eleven","twelve","thirteen","fourteen","fifteen","seventeen","eighteen","nineteen"};
+    	String twentyTo90[]={"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+    	
         int minus = 0;
         while(n!=0){
-        	minus=0;
+       	if(n>=1000000&&n<1000000000){
+       		s+=zeroTo999(n/1000000,zeroTo9,tenTo19,twentyTo90)+" million";
+       		minus = (n/1000000)*1000000;
+       	}
         if(n>=1000&&n<1000000){
-        	sub =n;
-        	
-        	//s+= zeroToNine[n/1000]+" thousand";
-        	n = n/1000;
-        	minus += (n/1000)*1000;
+        	s+=zeroTo999(n/1000,zeroTo9,tenTo19,twentyTo90)+" thousand";
+        	minus = (n/1000)*1000;
         }
-        if(n>=100&&n<1000){	
-        	s+= zeroToNine[n/100]+" hundred";
-        	minus+=(n/100)*100;
+   
+        if(0<n&&n<1000){
+        	s+=zeroTo999(n,zeroTo9,tenTo19,twentyTo90);
+        	minus=n;
         }
-        if(n>=20&&n<100){
-     		s+=tenToNinety[n/10];
-     		 minus+= (n/10)*10;
-     	}
-    	if(n>=10&&n<20){
-    		 s+=tenToNineteen[n%10];
-    		 minus+= n;
-    	 }
-    	if(n>0&&n<10){
-    		s+=zeroToNine[n];
-    		minus+= n;
-    	}
-    
     	n-=minus;
-    	n += sub;
     	if(n!=0) s+=" ";
         }
         return s;
     }
-    public static String zeroTo999(int n,String[] zeroToNine,String[] tenToNineteen,String[] tenToNinety){
+    public static String zeroTo999(int n,String[] zeroToNine,String[] tenToNineteen,String[] twentyToNinety){
     	String s="";
+    	int minus = 0;
+    	while(n!=0){
     	  if(n>=100&&n<1000){	
           	s+= zeroToNine[n/100]+" hundred";
-          	n-=(n/100)*100;
+          	minus=(n/100)*100;
           }
           if(n>=20&&n<100){
-       		s+=tenToNinety[n/10];
-       		n-= (n/10)*10;
+       		s+=twentyToNinety[n/10];
+       		minus= (n/10)*10;
        	}
       	if(n>=10&&n<20){
       		 s+=tenToNineteen[n%10];
-      		 n-= n;
+      		 minus= n;
       	 }
       	if(n>0&&n<10){
       		s+=zeroToNine[n];
-      		n-= n;
+      		minus= n;
       	}
+      	n-=minus;
       	if(n!=0) s+=" ";
+    	}
           return s;
     }
 }
