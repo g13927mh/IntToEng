fb6876dd804f9f9241270a2eef82045f06aa4a98
g13927mh <g13927mh@s30402.tsuda.ac.jp>
2015-05-08 11:25:58 +0900

    0~9の場合のテストケースをつくりました。村上

---
+++ src/IntToEng.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class IntToEng {
+
+    // メインメソッド
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        System.out.println(translateEng(input));
+
+    }
+
+    // 数値を英訳する変換するメソッド
+    static String translateEng(int n) {
+    	if(n<0||n>10) return "範囲外です";
+    	
+    	
+        return "";
+    }
+}
